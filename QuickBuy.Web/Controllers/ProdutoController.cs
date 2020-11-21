809a07b916d85ea8a6007d0cf9d8492e65a43b49
Isaque <isaque_dornelas@hotmail.com>
2020-11-21 19:40:09 -0300

    Inserção de algumas conexões com a web.api e o banco de dados

---
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+                //if(condicao == false)
+                //{
+                //    return BadRequest("Porque retornou uma conexão mal sucedida");
+                //}
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            try
+            {
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
